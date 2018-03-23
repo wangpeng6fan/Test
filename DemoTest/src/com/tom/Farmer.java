112d127ada52e129bd51dfed87b66bb24b05f26a
wangpeng6fan <wangpeng5fan@sina.com>
2018-03-23 14:21:05 +0800

    fix farm

--- DemoTest/src/com/tom/Farmer.java
+++ DemoTest/src/com/tom/Farmer.java
@@ -9,6 +9,12 @@ public class Farmer extends People{
 		super.eat();
 		System.out.println("子类1吃得快");
 	}
+	
+	public void eat(){
+		
+		
+		System.out.println("吃得慢");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           Farmer f=new Farmer();
