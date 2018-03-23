e6627bbffcf1d9dd79264ec30e8cd21df0b6cff3
wangpeng6fan <wangpeng5fan@sina.com>
2018-03-23 15:49:41 +0800

    fix nnnnnn

--- DemoTest/src/com/tom/Farmer.java
+++ DemoTest/src/com/tom/Farmer.java
@@ -9,23 +9,6 @@ public class Farmer extends People{
 		super.eat();
 		System.out.println("子类1吃得快");
 	}
-	
-	public void eat(){
-		
-		
-		System.out.println("吃得慢");
-	}
-
-	
-	private String name;
-	
-
-
-
-	
-	
-	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           Farmer f=new Farmer();
