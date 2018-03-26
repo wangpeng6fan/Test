7655510296445e93ff3c8eed14dafeff213415c0
wangpeng6fan <wangpeng5fan@sina.com>
2018-03-26 08:35:04 +0800

    fix method

--- DemoTest/src/com/tom/Farmer.java
+++ DemoTest/src/com/tom/Farmer.java
@@ -1,43 +1,67 @@
-package com.tom;
+﻿package com.tom;
+
 
 import com.tx.People;
 
+
 public class Farmer extends People{
 
+	
 	public void eatt(){
 		
-		super.eat();
-		System.out.println("子类1吃得快");
-	}
+	
+	super.eat();
+	
+	System.out.println("子类1吃得快");
+	
+}
+	
 	
 	public void eat(){
-		
-		
-		System.out.println("吃得慢");
-	}
-	public void drink(){
-		
-		System.out.println("喝的快");
-	}
-	public void shut(){
-		
-		System.out.println("喊得响");
-	}	
-
-	
-	private String name;
-	
-
-
-
-	
 	
 	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          Farmer f=new Farmer();
-          f.eatt();
-	}
+	System.out.println("吃得慢");
+	
 
 }
+	
+	public void drink(){
+		
+	
+	System.out.println("喝的快");
+	
+}
+	
+	public void shut(){
+		
+
+
+		System.out.println("喊得响");
+	
+}	
+
+	
+private String name;
+	
+
+
+
+	
+	
+	
+
+
+	public static void main(String[] args) {
+	
+	// TODO Auto-generated method stub
+       
+   Farmer f=new Farmer();
+        
+  f.eatt();
+	
+}
+
+
+}
+
